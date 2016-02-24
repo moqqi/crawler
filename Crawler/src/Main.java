986ce5d21a6f8ba79c99fc9ed34663998af9e301
moqqi <rnewson@dal.ca>
2016-02-24 10:47:00 -0400

    Added files via upload

---
+++ Crawler/src/Main.java
@@ -0,0 +1,51 @@
+import java.io.IOException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+/**
+ * Created by iMoqii on 16-02-23.
+ */
+public class Main {
+    public static DB db = new DB();
+
+    public static void main(String[] args) throws SQLException, IOException{
+        db.runSql2("Truncate Record;");
+        processPage("http://www.bookstore.dal.ca/Course/campus");
+    }
+    public static void processPage(String URL) throws SQLException, IOException {
+        //check if the URL is in DB.
+        String sql = "select * from Record where URL = '"+URL+"'";
+        ResultSet rs = db.runSql(sql);
+        if (rs.next()){
+
+        }else{
+            //store url to DB to avoid multiple parsing.
+            sql = "INSERT INTO  `Crawler`.`Record` " + "(`URL`) VALUES " + "(?);";
+            PreparedStatement stmt = db.conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            stmt.setString(1, URL);
+            stmt.execute();
+
+            //get useful information
+            Document doc = Jsoup.connect(URL).get();
+            if (doc.text().contains("")){
+                System.out.println(URL);
+            }
+
+            //get all links and recursively call the processPage method
+            Elements questions = doc.select("a[href]");
+            for (Element link: questions){
+                if(link.attr("href").contains(""))
+                    processPage(link.attr("abs:href"));
+
+            }
+
+
+        }
+    }
+}
